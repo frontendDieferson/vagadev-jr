586d1e61ab609f13e809303f9b4f7518afee7344
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2021-10-07 00:19:25 -0300

    Features Carousel Finished

---
+++ n1rush/src/Components/Button/style.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const PurchaseButton = styled.button`
+  position: relative !important;
+  width: 240px !important;
+  height: 52px !important;
+  all: unset;
+  background-color: #3ec6e0;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform: transition all 0.5s;
+  margin: auto auto !important;
+  
+  p {
+    color: #ffffff;
+    font-weight: 900;
+    text-transform: uppercase;
+    font-size: 18px;
+    margin-right: 0px !important;
+    
+  }
+  img {
+    position: absolute;      
+    margin-left: 123px !important;
+    bottom: 0;
+    width: 68px;
+    height: 79px;
+    display: none;
+    display: none;   
+  }
+
+  `
--- n1rush/src/pages/Home/index.js
+++ n1rush/src/pages/Home/index.js
@@ -4,28 +4,28 @@ import TopBar from "../../components/topBar";
 import HighlightIcon from "../../assets/svgs/highlight-icon.svg";
 import { Title, Products, App } from "./style";
 import Footer from "../../components/footer";
-import React from "react";
+import React, {useState} from "react";
 import ProductListDesktop from "../../components/productList";
 import ProductListMobile from "../../components/productListMobile";
 
 
 const Home = () => {
 
-  const [bagTotal, setBagTotal] = React.useState(0);
+  const [total, setTotal] = useState(0);
   
   return (
     
     <App>
       <Carousel />
-      <TopBar bagTotal={bagTotal}/>
+      <TopBar total={total}/>
       <MediumBanners />
       <Products>
         <Title>
           <img src={HighlightIcon} alt="icon" />
           <h2> Produtos em destaque </h2>
         </Title>
-        <ProductListDesktop setBagTotal={setBagTotal} bagTotal={bagTotal}/>
-        <ProductListMobile setBagTotal={setBagTotal} bagTotal={bagTotal}/>
+        <ProductListDesktop setTotal={setTotal} bagTotal={bagTotal}/>
+        <ProductListMobile setTotal={setTotal} bagTotal={bagTotal}/>
       </Products>     
       <Footer/> 
     </App>
